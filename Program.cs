cbf2518da73d6aefd8ac58fbdb77bb23a2b26fab
dylanjcassidy <dylan.j.cassidy@uconn.edu>
2023-01-05 10:35:30 -0500

    Can now write to .ppm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+void Render(int h, int w)
+{
+    string fileName = "out.ppm";
+    string curDirectory = Directory.GetCurrentDirectory();
+    Console.WriteLine(curDirectory);
+    using (StreamWriter outputFile = new StreamWriter(Path.Combine(curDirectory, fileName)))
+    {
+        outputFile.WriteLine("P3");
+        outputFile.WriteLine(h + " " + w);
+        outputFile.WriteLine("255");
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                outputFile.WriteLine("0 0 0");
+            }
+        }
+    }
+}
+
+Render(256, 256);
